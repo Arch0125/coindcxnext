6164e662d9a5abca766579bd51105a750dcb1d35
Arch0125 <dasarchisman25@gmail.com>
2022-05-29 09:04:51 +0530

    Add web3 connector

--- pages/HomeSwap.js
+++ pages/HomeSwap.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import { Box, Flex, Text, Input, Select, Button} from '@chakra-ui/react';
 import { Divider } from '@chakra-ui/react';
+import Web3 from 'web3';
+import { useEffect, useState } from 'react';
 
 function HomeSwap() {
+
+    const[currentAccount,setCurrentAccount]=useState('');
+    const[pvtKey,setPvtKey]=useState('');
+
+    const checkWalletHandler= async() =>{
+        const {ethereum} = window;
+        if(!ethereum){
+            console.log("metamask detected");
+            return;
+        }
+        try{
+            const accounts = await ethereum.request({method : 'eth_requestAccounts'});
+            setCurrentAccount(accounts[0]);
+            console.log("Install Metamask")
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+
     return ( 
         <>
         <Box width={"100%"} textAlign={"center"} height={"fit-content"} bgColor={"#2c2c2c"}  >
@@ -11,7 +33,9 @@ function HomeSwap() {
         <Flex bgColor={"#050100"} height={"fit-content"} flexDirection={"row"} >
             
             <Box margin={"40px"} width={'50%'}  >
+                
                 <Box padding={"20px"} width={"80%"} height={"fit-content"} bgColor={"#242124"} borderRadius={"30px"}>
+                <Text textAlign={"center"} fontWeight={"bold"} color={'white'} >Uniswap Offline Portal</Text>
                     <Text color={"white"} >From Token</Text>
                     <Select placeholder='Select From token' color={"white"}  >
                     <option value='option1'>ETH</option>
@@ -30,6 +54,8 @@ function HomeSwap() {
 
                 </Box>
                 <Box padding={"20px"} mt={"100px"} width={"80%"} height={"fit-content"} bgColor={"#242124"} borderRadius={"30px"}>
+                <Text textAlign={"center"} fontWeight={"bold"} color={'white'} >Binance Offline Portal</Text>
+
                     <Text color={"white"} >From Token</Text>
                     <Select placeholder='Select From token' color={"white"}  >
                     <option value='option1'>ETH</option>
@@ -51,8 +77,10 @@ function HomeSwap() {
             <Box>
                 <Box mt={"35px"}  flexDirection={"column"} justifyContent={"center"} alignContent={"center"} padding={"30px"} textAlign={"center"} color={"white"} bgColor={"#2c2c2c"}  width={"500px"} height={"fit-content"} borderRadius={"30px"} >
                     <Text fontSize={"20px"} fontWeight={"bold"} >Current Public Key</Text>
-                    <Text fontSize={"20px"} fontWeight={"bold"} >0xE4F67</Text>
-                    <Button variant={"solid"} colorScheme={"whiteAlpha"} alignSelf={"center"} mt={"10px"}>Generate Private Key</Button>
+                    <Text fontSize={"20px"} fontWeight={"bold"} >{currentAccount.substring(0,7)+"..."+currentAccount.substring(37)}</Text>
+                    <Button variant={"solid"} colorScheme={"whiteAlpha"} alignSelf={"center"} mt={"10px"} onClick={checkWalletHandler} >Get Current Account</Button>
+                    <Input variant={"outline"} mt={"10px"} placeholder="Enter your private key" />
+                    <Button variant={"solid"} placeholder="Enter Private Key" colorScheme={"whiteAlpha"} alignSelf={"center"} mt={"10px"}>Set Private Key</Button>
 
                 </Box>
                 <Box mt={"35px"}  flexDirection={"column"} justifyContent={"center"} alignContent={"center"} padding={"30px"} textAlign={"center"} color={"white"} bgColor={"#2c2c2c"}  width={"500px"} height={"fit-content"} borderRadius={"30px"} >
