fcc3195c286a4c0a8daade8fa7f7f4dbc64446b6
Arch0125 <dasarchisman25@gmail.com>
2022-05-29 09:29:09 +0530

    Add uniswap offline

--- pages/HomeSwap.js
+++ pages/HomeSwap.js
@@ -8,6 +8,10 @@ function HomeSwap() {
 
     const[currentAccount,setCurrentAccount]=useState('');
     const[pvtKey,setPvtKey]=useState('');
+    const[UniToken,setUniToken]=useState('');
+    const[toUniToken,setToUniToken]=useState('');
+    const[BnbToken,setBnbToken]=useState('');
+    const[toBnbToken,setToBnbToken]=useState('');
 
     const checkWalletHandler= async() =>{
         const {ethereum} = window;
@@ -24,6 +28,22 @@ function HomeSwap() {
         }
     }
 
+    const UniswapOffline = async(pvtKey) => {
+        let web3 = new Web3;
+        var tx = {
+            nonce: 1,
+            gasPrice: 25000,
+            gasLimit: 25000,
+            to: 'contractAddress',
+            value: 1000000,
+            data: 'contractData',
+            chainId : 1,
+        };
+
+        let acc = await web3.eth.accounts.wallet.add(pvtKey);
+        console.log(acc);
+    }
+
 
     return ( 
         <>
@@ -37,20 +57,20 @@ function HomeSwap() {
                 <Box padding={"20px"} width={"80%"} height={"fit-content"} bgColor={"#242124"} borderRadius={"30px"}>
                 <Text textAlign={"center"} fontWeight={"bold"} color={'white'} >Uniswap Offline Portal</Text>
                     <Text color={"white"} >From Token</Text>
-                    <Select placeholder='Select From token' color={"white"}  >
-                    <option value='option1'>ETH</option>
-                    <option value='option2'>DAI</option>
-                    <option value='option3'>UNI</option>
+                    <Select onChange={e => setUniToken(e.target.value)} placeholder='Select From token' color={"white"}  >
+                    <option value='0xc778417E063141139Fce010982780140Aa0cD5Ab'>ETH</option>
+                    <option value='0xaD6D458402F60fD3Bd25163575031ACDce07538D'>DAI</option>
+                    <option value='0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984'>UNI</option>
                     </Select>
                     <Input placeholder='$0.00' variant={"outline"} mt={"15px"} bgColor={"#292929"} borderColor={"blackAlpha.700"} color={"white"} _hover={"none"} _active={"none"} />
                     <Text mt={"50px"} color={"white"} >To Token</Text>
-                    <Select color={"white"} placeholder='Select to Token'>
-                    <option value='option1'>ETH</option>
-                    <option value='option2'>DAI</option>
-                    <option value='option3'>UNI</option>
+                    <Select onChange={e =>setToUniToken(e.target.value)} color={"white"} placeholder='Select to Token'>
+                    <option value='0xc778417E063141139Fce010982780140Aa0cD5Ab'>ETH</option>
+                    <option value='0xaD6D458402F60fD3Bd25163575031ACDce07538D'>DAI</option>
+                    <option value='0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984'>UNI</option>
                     </Select>
                     <Input placeholder='$0.00' variant={"outline"} mt={"15px"} bgColor={"#292929"} borderColor={"blackAlpha.700"} color={"white"} _hover={"none"} _active={"none"} />
-                    <Button variant={"solid"} colorScheme={"whiteAlpha"} alignSelf={"center"} mt={"10px"} ml={"10vw"} >Sign Offline Transaction</Button>
+                    <Button onClick={UniswapOffline} variant={"solid"} colorScheme={"whiteAlpha"} alignSelf={"center"} mt={"10px"} ml={"10vw"} >Sign Offline Transaction</Button>
 
                 </Box>
                 <Box padding={"20px"} mt={"100px"} width={"80%"} height={"fit-content"} bgColor={"#242124"} borderRadius={"30px"}>
@@ -79,9 +99,8 @@ function HomeSwap() {
                     <Text fontSize={"20px"} fontWeight={"bold"} >Current Public Key</Text>
                     <Text fontSize={"20px"} fontWeight={"bold"} >{currentAccount.substring(0,7)+"..."+currentAccount.substring(37)}</Text>
                     <Button variant={"solid"} colorScheme={"whiteAlpha"} alignSelf={"center"} mt={"10px"} onClick={checkWalletHandler} >Get Current Account</Button>
-                    <Input variant={"outline"} mt={"10px"} placeholder="Enter your private key" />
-                    <Button variant={"solid"} placeholder="Enter Private Key" colorScheme={"whiteAlpha"} alignSelf={"center"} mt={"10px"}>Set Private Key</Button>
-
+                    <Input onChange={e => setPvtKey(e.target.value)} variant={"outline"} mt={"10px"} placeholder="Enter your private key" />
+                    <Button variant={"solid"} placeholder="Enter Private Key" colorScheme={"whiteAlpha"} alignSelf={"center"} mt={"10px"} >Set Private Key</Button>
                 </Box>
                 <Box mt={"35px"}  flexDirection={"column"} justifyContent={"center"} alignContent={"center"} padding={"30px"} textAlign={"center"} color={"white"} bgColor={"#2c2c2c"}  width={"500px"} height={"fit-content"} borderRadius={"30px"} >
                     <Text fontSize={"20px"} fontWeight={"bold"} >Current Offline Signed Transaction</Text>
